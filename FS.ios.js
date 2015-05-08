583512e345d6e2214478db3799e2c0d3c6bd7b00
beastrc <makarov0114@outlook.com>
2015-05-09 00:17:10 +0300

    Added unlink suport

--- FS.ios.js
+++ FS.ios.js
@@ -7,9 +7,15 @@ var base64 = require('base-64');
 var _readDir = Promise.promisify(RNFSManager.readDir);
 var _stat = Promise.promisify(RNFSManager.stat);
 var _readFile = Promise.promisify(RNFSManager.readFile);
+var _writeFile = Promise.promisify(RNFSManager.writeFile);
+var _unlink = Promise.promisify(RNFSManager.unlink);
 
 var convertError = (err) => {
-  var error = new Error(err.description);
+  if (err.isOperational) {
+    err = err.cause;
+  }
+
+  var error = new Error(err.description || err.message);
   error.code = err.code;
   throw error;
 };
@@ -21,22 +27,22 @@ var RNFS = {
 
   readDir(path, rootDir) {
     return _readDir(path, rootDir)
-    .catch(convertError);
+      .catch(convertError);
   },
 
   stat(filepath) {
     return _stat(filepath)
-    .then((result) => {
-      return {
-        'ctime': new Date(result.ctime*1000),
-        'mtime': new Date(result.mtime*1000),
-        'size': result.size,
-        'mode': result.mode,
-        isFile: () => result.type === NSFileTypeRegular,
-        isDirectory: () => result.type === NSFileTypeDirectory,
-      };
-    })
-    .catch(convertError);
+      .then((result) => {
+        return {
+          'ctime': new Date(result.ctime*1000),
+          'mtime': new Date(result.mtime*1000),
+          'size': result.size,
+          'mode': result.mode,
+          isFile: () => result.type === NSFileTypeRegular,
+          isDirectory: () => result.type === NSFileTypeDirectory,
+        };
+      })
+      .catch(convertError);
   },
 
   readFile(filepath, shouldDecode) {
@@ -49,6 +55,16 @@ var RNFS = {
     }
 
     return p.catch(convertError);
+  },
+
+  writeFile(filepath, contents, options) {
+    return _writeFile(filepath, base64.encode(contents), options)
+      .catch(convertError);
+  },
+
+  unlink(filepath) {
+    return _unlink(filepath)
+      .catch(convertError);
   },
 
   MainBundle: RNFSManager.MainBundleDirectory,
--- RNFSManager.m
+++ RNFSManager.m
@@ -29,54 +29,82 @@ RCT_EXPORT_METHOD(readDir:(NSString*)directory inFolder:(NSNumber*)folder callba
     NSArray *paths = NSSearchPathForDirectoriesInDomains(folderInt, NSUserDomainMask, YES);
     path = [paths objectAtIndex:0];
   }
-  
+
   NSFileManager *fileManager = [NSFileManager defaultManager];
   NSError *error;
   NSString * dirPath = [path stringByAppendingPathComponent:directory];
   NSArray *contents = [fileManager contentsOfDirectoryAtPath:dirPath error:&error];
-  
+
   contents = [contents mapObjectsUsingBlock:^id(id obj, NSUInteger idx) {
     return @{
       @"name": (NSString*)obj,
       @"path": [dirPath stringByAppendingPathComponent:(NSString*)obj]
     };
   }];
-  
+
   if (error) {
     return callback([self makeErrorPayload:error]);
   }
-  
+
   callback(@[[NSNull null], contents]);
 }
 
 RCT_EXPORT_METHOD(stat:(NSString*)filepath callback:(RCTResponseSenderBlock)callback){
   NSError *error;
   NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filepath error:&error];
-  //  NSLog(@"%@", attributes);
-  
+
   if (error) {
     return callback([self makeErrorPayload:error]);
   }
-  
+
   attributes = @{
-                 @"ctime": [self dateToTimeIntervalNumber:(NSDate*)[attributes objectForKey:NSFileCreationDate]],
-                 @"mtime": [self dateToTimeIntervalNumber:(NSDate*)[attributes objectForKey:NSFileModificationDate]],
-                 @"size": [attributes objectForKey:NSFileSize],
-                 @"type": [attributes objectForKey:NSFileType],
-                 @"mode": [NSNumber numberWithInteger:[[NSString stringWithFormat:@"%o", [(NSNumber*)[attributes objectForKey:NSFilePosixPermissions] integerValue]] integerValue]]
-                 };
-  
+    @"ctime": [self dateToTimeIntervalNumber:(NSDate*)[attributes objectForKey:NSFileCreationDate]],
+    @"mtime": [self dateToTimeIntervalNumber:(NSDate*)[attributes objectForKey:NSFileModificationDate]],
+    @"size": [attributes objectForKey:NSFileSize],
+    @"type": [attributes objectForKey:NSFileType],
+    @"mode": [NSNumber numberWithInteger:[[NSString stringWithFormat:@"%o", [(NSNumber*)[attributes objectForKey:NSFilePosixPermissions] integerValue]] integerValue]]
+  };
+
   callback(@[[NSNull null], attributes]);
+}
+
+RCT_EXPORT_METHOD(writeFile:(NSString*)filepath contents:(NSString*)base64Content attributes:(NSDictionary*)attributes callback:(RCTResponseSenderBlock)callback){
+  NSData *data = [[NSData alloc] initWithBase64EncodedString:base64Content options:NSDataBase64DecodingIgnoreUnknownCharacters];
+  BOOL success = [[NSFileManager defaultManager] createFileAtPath:filepath contents:data attributes:attributes];
+
+  if (!success) {
+    return callback(@[[NSString stringWithFormat:@"Could not write file at path %@", filepath]]);
+  }
+
+  callback(@[[NSNull null], [NSNumber numberWithBool:success]]);
+}
+
+RCT_EXPORT_METHOD(unlink:(NSString*)filepath callback:(RCTResponseSenderBlock)callback) {
+  NSFileManager *manager = [NSFileManager defaultManager];
+  BOOL exists = [manager fileExistsAtPath:filepath isDirectory:false];
+
+  if (!exists) {
+    return callback(@[[NSString stringWithFormat:@"File at path %@ does not exist", filepath]]);
+  }
+  NSError *error;
+  BOOL success = [manager removeItemAtPath:filepath error:&error];
+
+  if (!success) {
+    NSLog(@"%@", error);
+    return callback([self makeErrorPayload:error]);
+  }
+
+  callback(@[[NSNull null], [NSNumber numberWithBool:success], filepath]);
 }
 
 RCT_EXPORT_METHOD(readFile:(NSString*)filepath callback:(RCTResponseSenderBlock)callback){
   NSData *content = [[NSFileManager defaultManager] contentsAtPath:filepath];
   NSString *base64Content = [content base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
-  NSLog(@"%@", base64Content);
+
   if (!base64Content) {
     return callback(@[[NSString stringWithFormat:@"Could not read file at path %@", filepath]]);
   }
-  
+
   callback(@[[NSNull null], base64Content]);
 }
 
