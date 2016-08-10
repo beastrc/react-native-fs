362c8fb3d0be18f02bc4ab2631d02b8c1e402896
beastrc <makarov0114@outlook.com>
2016-08-10 19:53:24 +0100

    Removed NS prefixes to avoid confusion

--- RNFSManager.m
+++ RNFSManager.m
@@ -253,7 +253,7 @@ RCT_EXPORT_METHOD(downloadFile:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-  DownloadParams* params = [DownloadParams alloc];
+  RNFSDownloadParams* params = [RNFSDownloadParams alloc];
 
   NSNumber* jobId = options[@"jobId"];
   params.fromUrl = options[@"fromUrl"];
@@ -295,7 +295,7 @@ RCT_EXPORT_METHOD(downloadFile:(NSDictionary *)options
 
   if (!self.downloaders) self.downloaders = [[NSMutableDictionary alloc] init];
 
-  Downloader* downloader = [Downloader alloc];
+  RNFSDownloader* downloader = [RNFSDownloader alloc];
 
   [downloader downloadFile:params];
 
@@ -304,7 +304,7 @@ RCT_EXPORT_METHOD(downloadFile:(NSDictionary *)options
 
 RCT_EXPORT_METHOD(stopDownload:(nonnull NSNumber *)jobId)
 {
-  Downloader* downloader = [self.downloaders objectForKey:[jobId stringValue]];
+  RNFSDownloader* downloader = [self.downloaders objectForKey:[jobId stringValue]];
 
   if (downloader != nil) {
     [downloader stopDownload];
@@ -315,7 +315,7 @@ RCT_EXPORT_METHOD(uploadFiles:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-  UploadParams* params = [UploadParams alloc];
+  RNFSUploadParams* params = [RNFSUploadParams alloc];
 
   NSNumber* jobId = options[@"jobId"];
   params.toUrl = options[@"toUrl"];
@@ -356,7 +356,7 @@ RCT_EXPORT_METHOD(uploadFiles:(NSDictionary *)options
 
   if (!self.uploaders) self.uploaders = [[NSMutableDictionary alloc] init];
 
-  Uploader* uploader = [Uploader alloc];
+  RNFSUploader* uploader = [RNFSUploader alloc];
 
   [uploader uploadFiles:params];
 
@@ -365,7 +365,7 @@ RCT_EXPORT_METHOD(uploadFiles:(NSDictionary *)options
 
 RCT_EXPORT_METHOD(stopUpload:(nonnull NSNumber *)jobId)
 {
-  Uploader* uploader = [self.uploaders objectForKey:[jobId stringValue]];
+  RNFSUploader* uploader = [self.uploaders objectForKey:[jobId stringValue]];
 
   if (uploader != nil) {
     [uploader stopUpload];
@@ -443,14 +443,14 @@ RCT_EXPORT_METHOD(getFSInfo:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromise
 - (NSDictionary *)constantsToExport
 {
   return @{
-           @"MainBundlePath": [[NSBundle mainBundle] bundlePath],
-           @"NSCachesDirectoryPath": [self getPathForDirectory:NSCachesDirectory],
-           @"NSDocumentDirectoryPath": [self getPathForDirectory:NSDocumentDirectory],
-           @"NSExternalDirectoryPath": [NSNull null],
-           @"NSTemporaryDirectoryPath": NSTemporaryDirectory(),
-           @"NSLibraryDirectoryPath": [self getPathForDirectory:NSLibraryDirectory],
-           @"NSFileTypeRegular": NSFileTypeRegular,
-           @"NSFileTypeDirectory": NSFileTypeDirectory
+           @"RNFSMainBundlePath": [[NSBundle mainBundle] bundlePath],
+           @"RNFSCachesDirectoryPath": [self getPathForDirectory:NSCachesDirectory],
+           @"RNFSDocumentDirectoryPath": [self getPathForDirectory:NSDocumentDirectory],
+           @"RNFSExternalDirectoryPath": [NSNull null],
+           @"RNFSTemporaryDirectoryPath": NSTemporaryDirectory(),
+           @"RNFSLibraryDirectoryPath": [self getPathForDirectory:NSLibraryDirectory],
+           @"RNFSFileTypeRegular": NSFileTypeRegular,
+           @"RNFSFileTypeDirectory": NSFileTypeDirectory
            };
 }
 
