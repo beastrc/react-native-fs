04423c2d994c8a04ba73569cc573dacfbf98921c
beastrc <makarov0114@outlook.com>
2017-11-15 20:57:24 +0200

    Fix reference path to ReactNative.csproj

--- Downloader.m
+++ Downloader.m
@@ -51,7 +51,6 @@
   }
 
   config.HTTPAdditionalHeaders = _params.headers;
-  config.timeoutIntervalForRequest = [_params.readTimeout intValue] / 1000.0;
 
   _session = [NSURLSession sessionWithConfiguration:config delegate:self delegateQueue:nil];
   _task = [_session downloadTaskWithURL:url];
